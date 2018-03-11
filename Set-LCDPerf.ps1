7a1f8d2a0fba993031d7f86cebae2847ff64fbae
moullas <moullas@gmail.com>
2018-03-11 23:13:45 +0200

    Initial Commit

--- Set-LCDPerf.ps1
+++ Set-LCDPerf.ps1
@@ -1,6 +1,10 @@
-﻿Param(
-  [string]$mqHost,
-  [string]$mqTopic
+﻿[CmdletBinding()]
+Param(
+  [Parameter(Mandatory=$True,Position=1)]
+   [string]$mqHost,
+	
+   [Parameter(Mandatory=$True)]
+   [string]$mqTopic
 )
 
 $mqHost = 'darthubuntu'
@@ -8,21 +12,18 @@ $mqTopic = 'nodemcu/8824992'
 Function Update-LCD{
 #Date
 $nowdate = Get-date -format g
-
 #CPU
 $ProcessorStats = Get-WmiObject win32_processor -Property "LoadPercentage"
 $ComputerCpu = $ProcessorStats.LoadPercentage
-
 #Memory
 $OperatingSystem = Get-WmiObject win32_OperatingSystem -Property "FreePhysicalMemory","TotalVisibleMemorySize" 
 $FreeMemory = $OperatingSystem.FreePhysicalMemory
 $TotalMemory = $OperatingSystem.TotalVisibleMemorySize
 $MemoryUsed = ($FreeMemory/ $TotalMemory) * 100
-#$PercentMemoryUsed =  100 - ("{0:N2}" -f $MemoryUsed)
 $PercentMemoryUsed =  ("{0:N0}" -f (100 - $MemoryUsed)).PadLeft(3)
 
-
-#$mqMessage = '{\"line1\":\"19/02/2018\" ,\"line2\":\"CPU:100% MEM:90%\"}' #Works
+#Sample Message format
+#$mqMessage = '{\"line1\":\"19/02/2018\" ,\"line2\":\"CPU:100% MEM:90%\"}'
 
 $ComputerCPU = ($ComputerCpu.ToString()).PadLeft(3)
 
@@ -30,8 +31,6 @@ $mqMessage = "{\`"line1\`":\`"$nowDate\`" ,\`"line2\`":\`"CPU:$ComputerCpu%MEM:$
 write-verbose $mqMessage
 mosquitto_pub -h $mqHost -t $mqTopic -m $mqMessage --will-topic $mqTopic --will-payload "CLS"
 }
-
-#Update-LCD
 
 #Main App
 Register-EngineEvent PowerShell.Exiting –Action {mosquitto_pub -h $mqHost -t $mqTopic -m "CLS"}
